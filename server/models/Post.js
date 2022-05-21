b6c75be6d0e214e0b8a467e24e2897ad0bcdaadb
ishelly1997 <ishelly1997@gmail.com>
2022-05-21 19:43:17 -0400

    type defs checking mutations and qs

--- server/models/Post.js
+++ server/models/Post.js
@@ -26,7 +26,7 @@ const postSchema = new Schema(
         default: 1
       },
       liker: {
-        type: mongoose.schema.types.ObjectId,
+        type: Schema.Types.ObjectId,
         ref: 'User'
       }
     }]
