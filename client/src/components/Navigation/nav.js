57b8bc0da0220fd8ccc6b45d1e5a7fe026558a89
cesarsiguencia <cesar.siguencia@gmail.com>
2022-05-25 20:39:10 -0400

    .

---
+++ client/src/components/Navigation/nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom'
+
+const Nav = () => {
+    
+    return(
+        <nav className='navigator'>
+            <p className="tab-links">
+                <Link></Link>
+            </p>
+
+        </nav>
+    )
+}
