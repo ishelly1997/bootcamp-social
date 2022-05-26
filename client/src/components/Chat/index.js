bead1d85e3068a1e5c642f14c51a3cd91120a01c
cesarsiguencia <cesar.siguencia@gmail.com>
2022-05-26 04:08:17 -0400

    chat not working at the moment

--- client/src/components/Chat/index.js
+++ client/src/components/Chat/index.js
@@ -1,1 +1,9 @@
-//user direct messaging 
+import React from 'react'
+
+function ChatMessage(props){
+    const { text, uid } = props.message
+
+    return <p>{text}</p>
+}
+
+export default ChatMessage
--- client/src/pages/Messaging.js
+++ client/src/pages/Messaging.js
@@ -1,17 +1,75 @@
+import React, {useState }from 'react'
+import firebase from 'firebase/compat/app'
 
-import React from 'react'
+import 'firebase/firestore'
+
+import ChatMessage from '../components/Chat';
+
+// import { useAuthState } from 'react-firebase-hooks/auth';
+
+import { useCollectionData } from 'react-firebase-hooks/firestore'
+
+const firestore = firebase.firestore()
+
+firebase.initializeApp({
+    apiKey: "AIzaSyDCQ872GndGorK98N8kMCaXJ1sEggmYQ2A",
+    authDomain: "bootcamp-social.firebaseapp.com",
+    projectId: "bootcamp-social",
+    storageBucket: "bootcamp-social.appspot.com",
+    messagingSenderId: "931737071360",
+    appId: "1:931737071360:web:33692411258104eeb10de5",
+    measurementId: "G-E3DNN554NF"
+})
 
 
-const chatRoom = () => {
+
+const ChatRoom = () => {
     
-    
+    const messagesRef = firestore.collection('messages');
 
+    const query = messagesRef.orderBy('createdAt').limit(25)
+
+    const [messages] = useCollectionData(query, {idField: 'id'});
+
+    const [formValue, setFormValue] = useState('')
+
+    const sendMessage = async(event) => {
+        event.preventDefault();
+
+        // const { uid } = currentUser;
+
+        await messagesRef.add({
+            text: formValue,
+            createdAt: firebase.firestore.FieldValue.serverTimestamp()
+        })
+
+        setFormValue('');
+    }
 
     return(
-        <div class='chat-container'>
-            <p>hello</p>
-      </div>
-    )
+       <div>
+            <div>
+                {messages && messages.map(message => <ChatMessage key={message.id} message={message}/>)}
+            </div>
+    
+            <form onSubmit={sendMessage}>
+                <input value={formValue} onChange={(event) => setFormValue(event.target.value)}/>
+                <button type="submit">HEY
+                </button>
+            </form>
+         </div>
+          
+        )
+    
+      
+    
 }
 
-export default chatRoom
+export default ChatRoom
+
+
+
+
+
+
+    
