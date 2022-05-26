a1d6ff2c527b67241894977393902559f30aacf1
ishelly1997 <95631495+ishelly1997@users.noreply.github.com>
2022-05-26 11:49:54 -0400

    Revert "Revert "Develop""

---
+++ client/src/components/Navigation/nav.js
@@ -0,0 +1,25 @@
+// home
+// network
+// jobs
+// messaging
+// events
+// resource
+
+// import React from "react";
+
+// // import homeIcon from "../../images/homepage.png"
+
+// import { Link } from "react-router-dom";
+
+// const Nav = () => {
+//     return (
+//         <nav className="Navigator">
+//             <p className="links">
+//                 <Link id="navtab" to="/">Home </Link>
+
+//                 </p>
+//         </nav>
+//     )
+// }
+
+// export default Nav
---
+++ client/src/pages/Homepage.js
@@ -0,0 +1,22 @@
+
+import React from 'react';
+
+import Header from '../components/Header'
+
+const Home = () => {
+
+  // if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+  //   return <Navigate to="/" />;
+  // }
+  return (
+    <div>
+      <Header></Header>
+      <h3>Welcome Tech Alumns!</h3>
+
+      
+
+    </div>
+  );
+};
+
+export default Home;
