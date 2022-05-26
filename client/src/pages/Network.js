81befd1a86e08c0b6f5c589f74301c15f63e1ba7
ishelly1997 <ishelly1997@gmail.com>
2022-05-26 13:04:53 -0400

    network and profile

--- client/src/pages/Network.js
+++ client/src/pages/Network.js
@@ -1,11 +1,27 @@
 /* import React from "react";
+//import { Redirect, useParams } from 'react-router-dom';
+//import { useQuery } from '@apollo/client';
+//import { QUERY_USER } from '../utils/queries'
 
-const Network = ( ) => {
-    const username = user.username;
+import usericon from "../images/network.png"
+//import Auth from '../utils/auth';
+const Network = () => {
+  //const { data: userData } = useQuery(QUERY_USER)
     return (
-        <div>
-        <h3>{username}</h3>
+    <div className="container">
+{         {userData && userData.map(user => (
+          <div key={user._id} className="card mb-3">
+            <img src={usericon}></img>
+            <p className="card-header">
+              {user.username}
+            </p> }
+            <div className="card-body">
+              <p>Cohort: Columbia University Full-Stack Bootcamp May 2022</p>
+        <button id="view-profile">View Profile</button>
       </div>
+    </div>
+  ))};
+  </div>
     );
 };
 
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,21 +1,84 @@
 import React from 'react';
 
+
 const Profile = () => {
   return (
+    <div 
+    align="center"
+    >  
     <div>
-      <div className="flex-row mb-3">
-        <h2 className="bg-dark text-secondary p-3 display-inline-block">
-          {/* Viewing <usernames>'s profile. */}
-        </h2>
-      </div>
+        
+        <div>
+            
+            <div>
+                
+                <div>
+                <img 
+                style={{"border-radius":"90px"}}
+                src="https://i.imgur.com/bDLhJiP.jpg" 
+                width="200"  
+                alt='/'/>
+                </div>
+                
+                <div>
+                    <span>Recent Grad</span>
+                    <h5 >Lisa Bellum</h5>
+                    <span>Full Stack Software Developer</span>
+                    
+                    <div >
+                        <p>Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                    
+                    </div>
+                    
+                     <ul class="social-list">
+                        <li><i class="fa fa-facebook"></i></li>
+                        <li><i class="fa fa-dribbble"></i></li>
+                        <li><i class="fa fa-instagram"></i></li>
+                        <li><i class="fa fa-linkedin"></i></li>
+                        <li><i class="fa fa-google"></i></li>
+                    </ul>
+                    
+                    <div
+                    
+                    style={{"justify-content": "space-between"}}
+                    >
+                        
+                        <button
+                        style={{"background-color": "#fff", 
+                        "color": "#8E24AA",
+                        "border":"1px solid #8E24AA",
+                        "height":"40px",
+                        "width": "100px",
+                        "margin": "20px"      
+                      }}
+                        >
+                          Message
+                        </button>
+                        <button
+                        type='button'
+                        style={{"background-color": "#8E24AA", 
+                        "color": "#FFF",
+                        "border":"1px solid #8E24AA",
+                        "height":"40px", 
+                        "width": "100px",
+                        "margin": "20px"
+                        }}
+                        
+                        >Contact
+                        </button>
+                    </div>
+                    
+                    
+                </div>
+                
+               
+                
+                
+            </div>
 
-      <div className="flex-row justify-space-between mb-3">
-        <div className="col-12 mb-3 col-lg-8">{/* PRINT POST LIST  */}</div>
-
-        <div className="col-12 col-lg-3 mb-3">{/* PRINT USER.Email */}</div>
-        <div className='col-12'>Bootcamp Cohort December 2021 - May 2022</div>
+          </div>
+        </div>
       </div>
-    </div>
   );
 };
 
