c5222394e5bf9851227df4946f8b3dc8375d73d5
cesarsiguencia <cesar.siguencia@gmail.com>
2022-05-26 21:42:17 -0400

    added authentication to events page

--- client/src/pages/Events.js
+++ client/src/pages/Events.js
@@ -1,12 +1,34 @@
 import React from 'react';
 
-// import Auth from '../utils/auth'
+import Auth from '../utils/auth'
+
+import { Navigate, useParams } from 'react-router-dom';
+
+import { useQuery } from '@apollo/client';
+import { QUERY_USER, QUERY_ME } from '../utils/queries';
 
 const Events = () => {
+  const { username: userParam } = useParams();
 
-    // if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
-    //     return <Navigate to="/events" />;
-    //   }
+  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
+    variables: { username: userParam }
+  });
+
+  const user = data?.me || data?.user || {};
+
+
+  if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+    return <Navigate to="/profile" />;
+  }
+
+
+  if (!user?.username) {
+    return (
+      <h4>
+        You need to be logged in to see this page. Use the navigation links above to sign up or log in!
+      </h4>
+    );
+  }
 
   return (
     <div className="calendar">
