6a774a631c66912af727721ac5c2cb4c298c5c8a
cesarsiguencia <cesar.siguencia@gmail.com>
2022-05-26 23:50:49 -0400

    new server file

--- server/server.js
+++ server/server.js
@@ -7,32 +7,44 @@ const {authMiddleware} = require('./utils/auth');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
+
+// create a new Apollo server and pass in our schema data to the graphql documents
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+  //This would see the incoming request and return only the headers. On the resolver side, those headers would become the context parameter.
+});
+
 const app = express();
-
-const startServer = async () => {
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: authMiddleware,
-  });
-  await server.start();
-  server.applyMiddleware({ app });
-  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-};
-
-startServer()
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  // integrate our Apollo server with the Express application as middleware
+  server.applyMiddleware({ app });
 
 
+  // Serve up static assets for when in production, npm run build is only for production, use static from client build when production, in development, we use additonal dependencies, like noedmon, concurrently, so we don't need this during production
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
 
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
   });
-});
+  
+  db.once('open', () => {
+      app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`);
+        // log where we can go to test our GQL API
+        console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+      })
+    })
+  };
+  
+  // Call the async function to start the server
+  startApolloServer(typeDefs, resolvers);
