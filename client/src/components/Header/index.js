e3188e1540c95578de60bbcc3b20c2a647a0826e
cesarsiguencia <cesar.siguencia@gmail.com>
2022-05-30 13:36:26 -0400

    install concurrently

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -20,18 +20,3 @@ const Header = () => {
 };
 
 export default Header;
-
-
-
-{/* <header className="bg-secondary mb-4 py-2 flex-row align-center">
-<div className="container flex-row justify-space-between-lg justify-center align-center">
-  <Link to="/">
-    <h1>Bootcamp Social</h1>
-  </Link>
-  <img id="background"src={background} alt='logo'/>
-  <nav className="text-center">
-    <Link to="/login">Login</Link>
-    <Link to="/signup">Signup</Link>
-  </nav>
-</div>
-</header> */}
