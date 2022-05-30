eeb2d4248f6add73a15e187b9f4f80aeb1d63493
cesarsiguencia <cesar.siguencia@gmail.com>
2022-05-30 14:17:28 -0400

    new db

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/bootcamp-social-db',
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/bootcamp-social',
   {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
